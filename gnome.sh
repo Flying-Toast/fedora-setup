d6bd53efed0fe7f068291944205b0c9b0dca19df
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-02-22 17:05:23 -0500

    gnome: super-f to launch mail client

--- gnome.sh
+++ gnome.sh
@@ -40,6 +40,7 @@ dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/cus
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Terminal'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/www "['<Super>f']"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/email "['<Super>g']"
 dconf write /org/gnome/settings-daemon/plugins/color/night-light-enabled true
 dconf write /org/gnome/settings-daemon/plugins/color/night-light-temperature "uint32 3873"
 dconf write /org/gnome/shell/favorite-apps "@as []"
