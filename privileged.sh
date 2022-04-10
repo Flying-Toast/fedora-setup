4e5732577d8b6dd81b12834042ba0fa72d509127
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-04-10 09:52:55 -0400

    packages

--- privileged.sh
+++ privileged.sh
@@ -2,8 +2,8 @@
 
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
-awk '$1=="INSTALL"{print $2}' packages | xargs dnf install -y
-awk '$1=="REMOVE"{print $2}' packages | xargs dnf remove -y
+awk '$1=="+"{print $2}' packages | xargs dnf install -y
+awk '$1=="-"{print $2}' packages | xargs dnf remove -y
 
 ln -s /usr/bin/nvim /usr/bin/vim
 
