85a635bf733f9e69a6e66c916d1635a8ae57c867
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-08-13 11:43:43 -0400

    better vimrc install script

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -6,8 +6,8 @@ mkdir -p ~/.config/nvim
 touch ~/.config/nvim/init.vim
 mv ~/.config/nvim/init.vim init.vim.old
 
-#install vim-plug:
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+PLUG_INSTALL_TO=~/.local/share/nvim/site/autoload/plug.vim
+[ ! -e $PLUG_INSTALL_TO ] && curl -fLo $PLUG_INSTALL_TO --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cat config.vim >> ~/.config/nvim/init.vim
-vim -c "PlugInstall | PlugUpdate | qa"
+vim -c "PlugInstall | qa"
